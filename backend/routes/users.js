56c6cd9fb0e200d1b2fc0a92689f2595ee29ad1e
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-03 16:52:34 +0100

    Added tests and routes for GET requests Added routers to server.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,4 +5,14 @@ const userRouter = Router();
 
 // add user routes in here
 
+userRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).send(users);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 module.exports = userRouter;
