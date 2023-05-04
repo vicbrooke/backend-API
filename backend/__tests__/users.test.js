12dc508fdb6d2a3905f4d9fb7aa1d96597f1cbd6
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-04 11:59:07 +0100

    Added tests and routes for POST requests

--- backend/__tests__/users.test.js
+++ backend/__tests__/users.test.js
@@ -5,6 +5,7 @@ const app = require("../server");
 // db setup
 const seed = require("../db/seedFn");
 const { users } = require("../db/seedData");
+const { User } = require("../db/models");
 
 describe("users", () => {
   beforeAll(async () => {
@@ -12,16 +13,45 @@ describe("users", () => {
     await seed();
   });
 
-  it("should return a list of users", async () => {
-    // make a request
-    const response = await request(app).get("/users");
-    // assert a response code
-    expect(response.status).toBe(200);
-    // expect a response
-    expect(response.body).toBeDefined();
-    // toEqual checks deep equality in objects
-    expect(response.body[0]).toEqual(expect.objectContaining(users[0]));
-    expect(response.body[1]).toEqual(expect.objectContaining(users[1]));
-    expect(response.body[2]).toEqual(expect.objectContaining(users[2]));
+  describe("GET /users", () => {
+    it("should return a list of users", async () => {
+      // make a request
+      const response = await request(app).get("/users");
+      // assert a response code
+      expect(response.status).toBe(200);
+      // expect a response
+      expect(response.body).toBeDefined();
+      // toEqual checks deep equality in objects
+      expect(response.body[0]).toEqual(expect.objectContaining(users[0]));
+      expect(response.body[1]).toEqual(expect.objectContaining(users[1]));
+      expect(response.body[2]).toEqual(expect.objectContaining(users[2]));
+    });
+  });
+
+  describe("POST /users", () => {
+    const testUserData = {
+      username: "testuser",
+      name: "Test User",
+      password: "testing123",
+      email: "test.user@test.com",
+      avatar_URL: "https://example.com/avatar/testuser.jpg",
+    };
+    let response;
+    beforeEach(async () => {
+      response = await request(app).post("/users").send(testUserData);
+    });
+
+    it("should return the sent data", () => {
+      expect(response.body).toEqual(expect.objectContaining(testUserData));
+    });
+    it("returned user should match database entry", async () => {
+      const userInDb = await User.findOne({ where: { id: response.body.id } });
+      expect(userInDb.id).toEqual(response.body.id);
+      expect(userInDb.username).toEqual(response.body.username);
+      expect(userInDb.name).toEqual(response.body.name);
+      expect(userInDb.password).toEqual(response.body.password);
+      expect(userInDb.email).toEqual(response.body.email);
+      expect(userInDb.avatar_URL).toEqual(response.body.avatar_URL);
+    });
   });
 });
