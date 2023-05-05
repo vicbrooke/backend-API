0ba9b2c678e0613e01dd319b9fbd8728bb84ec17
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-05 12:28:49 +0100

    Updated function to hash passwords before they are saved in the database

--- backend/db/seedFn.js
+++ backend/db/seedFn.js
@@ -1,12 +1,22 @@
 const { db } = require("./db");
 const { Article, Comment, User } = require("./models");
+const bcrypt = require("bcrypt");
 
 const { articles, comments, users } = require("./seedData");
 
 const seed = async () => {
   try {
     await db.sync({ force: true }); // recreate db
-    const createdUsers = await User.bulkCreate(users);
+    const usersWithHashedPasswords = await Promise.all(
+      users.map(async (user) => {
+        const hash = await bcrypt.hash(user.password, 10);
+        return {
+          ...user,
+          password: hash,
+        };
+      })
+    );
+    const createdUsers = await User.bulkCreate(usersWithHashedPasswords);
     const createdArticles = await Article.bulkCreate(articles);
     const createdComments = await Comment.bulkCreate(comments);
   } catch (error) {
