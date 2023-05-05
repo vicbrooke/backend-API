6e0d8a604a0ed688ed5cbf35aec544f440e31d71
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-05 14:21:26 +0100

    Updated login route with new password and additional test Removed unused variables

--- backend/__tests__/login.test.js
+++ backend/__tests__/login.test.js
@@ -1,12 +1,9 @@
 const request = require("supertest");
 // express app
 const app = require("../server");
-const bcrypt = require("bcrypt");
 
 // db setup
 const seed = require("../db/seedFn");
-const { users } = require("../db/seedData");
-const { User } = require("../db/models");
 
 describe("login", () => {
   beforeAll(async () => {
@@ -17,25 +14,28 @@ describe("login", () => {
   describe("POST /login", () => {
     const loginInfo = {
       email: "joebloggs@example.com",
-      password: "password123",
+      password: "Password!23",
     };
     it("should return token", async () => {
       // make a request
       const response = await request(app).post("/login").send(loginInfo);
       // assert a response code
       expect(response.status).toBe(202);
-      // expect a response
-      expect(response.body).toBeDefined();
+      // expect a token response
+      expect(response.body.token).toBeDefined();
+      // expect a user repsonse
+      expect(response.body.user).toBeDefined();
     });
     it("should return a 401 status code and error message for invalid credentials", async () => {
       const invalidLoginInfo = {
         email: "invalid@example.com",
         password: "password123",
       };
-
+      // make a request
       const response = await request(app).post("/login").send(invalidLoginInfo);
-
+      // assert a response code
       expect(response.status).toBe(401);
+      // expect a response
       expect(response.text).toContain("Invalid email or password");
     });
   });
