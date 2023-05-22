3fdc9caaa4744fbf7c8a94a6dc5d186f8690ca09
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-22 11:39:56 +0100

    Created file to import from .env outside of src folder

---
+++ frontend/src/env.js
@@ -0,0 +1,8 @@
+const env = {
+  REACT_APP_DOMAIN: process.env.REACT_APP_DOMAIN,
+  REACT_APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+  REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI,
+  // Add other environment variables you need here
+};
+
+export default env;
