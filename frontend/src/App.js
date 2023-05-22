fcb5b3b183107a4e2c1f70f6392e66f6df3f4cea
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-22 11:37:11 +0100

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Articles from "./pages/Articles";
+import Navbar from "./layout/Navbar";
 
 function App() {
   return (
     <main>
-      <h1>This is my frontend application</h1>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/articles" element={<Articles />} />
+        </Routes>
+      </Router>
     </main>
   );
 }
