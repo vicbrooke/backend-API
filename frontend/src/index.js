5ebbd72542af06d0494fd4ca4d7491eb37ea2d13
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-22 11:40:17 +0100

    Added Auth0Provider wrapper

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import env from "./env"; // Import the env.js file
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={env.REACT_APP_DOMAIN}
+      clientId={env.REACT_APP_CLIENT_ID}
+      redirectUri="http://localhost:3000/articles"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
