7a19d432fdcc84c6002f6b1b884de25942c20ad6
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-05-25 11:31:54 +0100

    Moved files into backend folder

--- backend/middleware/oidcAuth.js
+++ backend/middleware/oidcAuth.js
@@ -1,5 +1,5 @@
 // provides authentication functionality using OpenID Connect
 const { auth } = require("express-openid-connect");
-const config = require("../../authConfig");
+const config = require("../authConfig");
 
 module.exports = auth(config);
